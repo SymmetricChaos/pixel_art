0cb38f3c1847ea688da68f65559de5180e95274d
SymmetricChaos <ajfraebel@gmail.com>
2021-12-12 17:18:05 -0500

    creating a trait for automata

---
+++ src/traits_and_structs/automata_trait.rs
@@ -0,0 +1,8 @@
+pub trait CellAutomata {
+    fn draw(&self, screen: &mut [u8]);
+    fn set_line(&mut self, x0: isize, y0: isize, x1: isize, y1: isize, alive: bool);
+
+    fn invert(&mut self);
+    fn clear(&mut self);
+    fn describe() -> String;
+}
